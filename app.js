0cb2948f223c0ed1dccd88a9ef9624772af80d00
Ignacio Lupi <ignacio.lupi@mercadolibre.com>
2018-12-04 14:56:55 -0300

    Adding handlebars to infowindow

--- app.js
+++ app.js
@@ -17,19 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/scripts/handlebars', express.static(__dirname + '/node_modules/handlebars/dist/'));
 
-app.get('/', function(req,res){
+app.get('/', function (req, res) {
   res.sendfile(__dirname + '/public/index.html');
- }); 
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
