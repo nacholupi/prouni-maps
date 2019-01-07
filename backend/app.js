95b2cf040bcd58543f75f52de184caec069751b8
Ignacio Lupi <ignacio.lupi@mercadolibre.com>
2019-01-07 16:33:40 -0300

    Launch with one command - npm start

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-var cors = require('cors');
+var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
@@ -13,12 +13,17 @@ mongoose.connect('mongodb://localhost:27017/project')
 var app = express();
 
 app.use(logger('dev'));
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+const __distDir = __dirname + '/../dist'
+app.use(express.static(__distDir));
 
 app.use('/api/project', projectRoute);
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__distDir, 'index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
