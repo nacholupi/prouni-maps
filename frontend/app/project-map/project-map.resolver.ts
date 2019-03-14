2c9dccbf9475bc68d09643d3036fb6ff39ef47e0
Ignacio Lupi <ignacio.lupi@mercadolibre.com>
2019-03-13 23:13:07 -0300

    lat-long shift

--- frontend/app/project-map/project-map.resolver.ts
+++ frontend/app/project-map/project-map.resolver.ts
@@ -27,16 +27,33 @@ export class ProjectMapResolver implements Resolve<any> {
                 (error) => { observer.error(error); },
                 () => {
                     this.service.getAll().subscribe(
-                        (value) => {
-                            result.projects = value;
+                        (projs) => {
+                            result.projects = projs;
                             observer.next(result);
                         },
                         (error) => { observer.error(error); },
                         () => {
-                            result.projects.forEach((pro) => {
-                                result.subjects.forEach((sub, i) => {
+                            const fixedProjs = new Array<Project>();
+                            result.projects.forEach((pro, i) => {
+
+                                if (fixedProjs.length !== 0) {
+                                    fixedProjs.forEach((newMarkers) => {
+
+                                        if (newMarkers.location.coordinates[0] === pro.location.coordinates[0] &&
+                                            newMarkers.location.coordinates[1] === pro.location.coordinates[1]) {
+
+                                            const a = 2 * Math.PI / result.projects.length;
+
+                                            pro.location.coordinates[0] = pro.location.coordinates[0] + .00004 * Math.cos(a * i);
+                                            pro.location.coordinates[1] = pro.location.coordinates[1] + .00004 * Math.sin(a * i);
+                                        }
+                                    });
+                                }
+                                fixedProjs.push(pro);
+
+                                result.subjects.forEach((sub, j) => {
                                     if (pro.subject === sub) {
-                                        pro.iconUrl = './assets/images/markers/place.' + i + '.svg';
+                                        pro.iconUrl = './assets/images/markers/place.' + j + '.svg';
                                     }
                                 });
                             });
